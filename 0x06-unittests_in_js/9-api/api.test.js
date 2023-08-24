2fcb9790a8c2b887161e68f16552e83b159e5724
OMmienu <nkedzi@gmail.com>
2023-08-24 14:19:54 +0000

    Regex integration testing

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,16 +1,17 @@
 const request = require('request');
 const { expect } = require('chai');
 
-describe('Index Page', () => {
-  it('should return a welcome message', (done) => {
-    request.get('http://localhost:7865', (err, res, body) => {
-      expect(res.statusCode).to.equal(200);
-      expect(body).to.equal('Welcome to the payment system');
-      done();
+describe('API', () => {
+  describe('Index Page', () => {
+    it('should return a welcome message', (done) => {
+      request.get('http://localhost:7865', (err, res, body) => {
+        expect(res.statusCode).to.equal(200);
+        expect(body).to.equal('Welcome to the payment system');
+        done();
+      });
     });
   });
-});
-describe('Cart Page', () => {
+  describe('Cart Page', () => {
     it('should return the correct status code when :id is a number', (done) => {
       request.get('http://localhost:7865/cart/5555', (err, res, body) => {
         expect(res.statusCode).to.equal(200);
